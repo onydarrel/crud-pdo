6f6e7788ea596b57ff3a45bc52f0130b3972f74f
onydarrel <onydarrell@gmail.com>
2020-08-13 09:59:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,33 +29,35 @@ $result = $db->query("SELECT * FROM users ORDER BY id ASC");
     <br /><br />
 
     <table class="table table-bordered table-hover table-sm">
-      <tr>
+      <thead class="table-success text-center">
         <th>Nama</th>
         <th>Umur</th>
         <th>Email</th>
         <th>Update</th>
-      </tr>
-      
-      <?php
-      while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        echo "<tr>";
-        echo "<td>" . $row['nama'] . "</td>";
-        echo "<td>" . $row['umur'] . "</td>";
-        echo "<td>" . $row['email'] . "</td>";
-        // echo "<td><a href=\"edit.php?id=$row[id]\">Edit</a> | 
-        // <a href=\"hapus.php?id=$row[id]\" onClick=\"return confirm('Apakah Anda yakin ingin menghapus?')\">Hapus</a></td>";        
-        echo "<td>
+      </thead>
+
+      <tbody>
+        <?php
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+          echo "<tr>";
+          echo "<td>" . $row['nama'] . "</td>";
+          echo "<td class='text-center'>" . $row['umur'] . "</td>";
+          echo "<td>" . $row['email'] . "</td>";
+          // echo "<td><a href=\"edit.php?id=$row[id]\">Edit</a> | 
+          // <a href=\"hapus.php?id=$row[id]\" onClick=\"return confirm('Apakah Anda yakin ingin menghapus?')\">Hapus</a></td>";        
+          echo "<td class='text-center'>
                 <div class='btn-group btn-group-sm'>
                   <a class='btn btn-secondary active' href=\"edit.php?id=$row[id]\">Edit</a>
                   <a class='btn btn-secondary' href=\"hapus.php?id=$row[id]\" onClick=\"return confirm('Apakah Anda yakin ingin menghapus?')\">Hapus</a>
                 </div>
               </td>";
-        echo "</tr>";
-      }
-      ?>
+          echo "</tr>";
+        }
+        ?>
+      </tbody>
     </table>
   </div>
-  
+
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 </body>
